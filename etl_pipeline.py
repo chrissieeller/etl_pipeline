3c6e3738102e6a235f28697850b6a72f64a3bf9e
Chríssie Eller <chrissie.eller@gmail.com>
2023-09-13 10:41:23 -0300

    add files to the repository

---
+++ etl_pipeline.py
@@ -0,0 +1,64 @@
+## EXTRACT
+
+sdw2023_api_url = 'http://sdw-2023-prd.up.railway.app'
+
+import pandas as pd
+
+df = pd.read_csv('project.csv')
+user_ids = df['UserID'].tolist()
+print(user_ids)
+
+import requests
+import json
+
+def get_user(id):
+    response = requests.get(f'{sdw2023_api_url}/users/{id}')
+    return response.json() if response.status_code == 200 else None
+
+users = [user for id in user_ids if (user := get_user(id)) is not None]
+print(json.dumps(users, indent=2))
+
+## TRANSFORM
+
+# !pip install openai 
+
+openai_api_key = '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'
+
+import openai
+
+openai.api_key = openai_api_key
+
+def generate_ai_news(user):
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {
+          "role": "system",
+          "content": "Você é um especialista em Marketing Bancário."
+      },
+      {
+          "role": "user",
+          "content": f"Crie uma mensagem para{user['name']} sobre a importância dos investimentos (máximo de 240 caracteres)"
+      }
+    ]
+  )
+  
+  return completion.choices[0].message.content.strip('\"')
+
+for user in users:
+  news = generate_ai_news(user)
+  print(news)
+  user['news'].append({
+      "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
+      "description": news
+  })
+
+  ## LOAD
+
+  def update_user(user):
+    response = requests.put(f"{sdw2023_api_url}/users/{user['id']}", json=user)
+    return True if response.status_code == 200 else False 
+
+for user in users:
+  success = update_user(user)
+  print(f"User {user['name']} updated? {success}!")
